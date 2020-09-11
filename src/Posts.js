c4e4868ac151c47d822e274715e3225d2497a1df
Pob <590650@gmail.com>
2020-09-12 01:31:42 +0700

    working on Posts.js component

--- src/Posts.js
+++ src/Posts.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API, Storage } from 'aws-amplify'
+import { listPosts } from './graphql/queries'
+import { onCreatePost } from './graphql/subscriptions'
 
 function Posts() {
-  return <div>View Posts</div>
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchPosts()
+    const subscription = API.graphql({ query: onCreatePost }).subscribe({
+      next: async (post) => {
+        const newPost = post.value.data.onCreatePost
+        const signedImg = await Storage.get(newPost.imageKey)
+        setPosts((prevPost) => [
+          { id: newPost.id, title: newPost.title, imageUrl: signedImg },
+          ...prevPost,
+        ])
+      },
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+
+  async function fetchPosts() {
+    const postData = await API.graphql({
+      query: listPosts,
+    })
+    const rawPosts = postData.data.listPosts.items
+    const posts = await Promise.all(
+      rawPosts.map(async ({ id, title, imageKey }) => {
+        const signedImg = await Storage.get(imageKey)
+        return { id, title, imageUrl: signedImg }
+      })
+    )
+    setPosts(posts)
+  }
+  return (
+    <div>
+      <h2 style={heading}>Posts</h2>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <img src={post.imageUrl} alt="" style={postImage} />
+            <h3>{post.title}</h3>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
+
+const heading = { margin: '20px 0' }
+const postImage = { width: 400 }
 
 export default Posts
